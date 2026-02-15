de693810bcdd8253516bed8b40142bc34da0b7a5
Isle <98317505+Plaguea@users.noreply.github.com>
2026-02-15 15:38:04 +0300

    Update and rename index.html to main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+// Connect to Supabase
+const supabaseUrl = "https://ibcsaiaxgfaqmowvoqmk.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImliY3NhaWF4Z2ZhcW1vd3ZvcW1rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzExNDg1ODksImV4cCI6MjA4NjcyNDU4OX0.yTmZ_XHxx5dQHrP1XISSkGs4bJ8YtEsUnfx-e48SY7c";
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
